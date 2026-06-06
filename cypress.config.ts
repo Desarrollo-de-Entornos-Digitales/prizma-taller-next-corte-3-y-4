fe7f305068f6e0180fbfc1fa4644e0604b974909
MatthwX <mathewalexander2023@gmail.com>
2026-06-06 15:48:10 -0500

    configure typress baseUrl and viewport

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  allowCypressEnv: false,
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        viewportWidth: 1280,
+        viewportHeight: 720,
+        video: false,
+        screenshotOnRunFailure: true,
+        setupNodeEvents(_on, _config) {
+            // no plugins necesarios por ahora
+        },
     },
-  },
 });
